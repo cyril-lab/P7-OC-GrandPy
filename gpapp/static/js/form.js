794ad0553c82bb494f750373a9e61d92bdbf4d65
Cyril Vernhes <c.vernhes3@gmail.com>
2020-10-05 17:24:03 +0200

    add code

--- gpapp/static/js/form.js
+++ gpapp/static/js/form.js
@@ -10,48 +10,48 @@ $(document).ready(function(){
             $.post(post_url, 
                 post_data, 
                 function(data, status){ 
-                console.log(data, status);
-                //I get the question
-                var question = $('#formtexte').val();
-                //question display
-                var $question = $("<div class='chat_right'><p>"+JSON.stringify(question)+"</p></div>");
-                //deletion of the form
-                $('#form-question').get(0).reset();
-                //adding the question to the screen
-                $('#chat').append($question);
-                //loader
-                $('.loader').fadeIn();
-                //adding the answer to the screen
-                var answer_papy = JSON.parse(data);
-                //answer api google map 
-                var $answer_address = $("<div class='chat_left'><p>"+answer_papy.address+"</p></div>");
-                //answer wikipedia
-                var $answer_wiki = $("<div class='chat_left'><p>"+
-                    answer_papy.history+"</p><a href='https://fr.wikipedia.org/wiki/"+
-                    answer_papy.article_title+"'>Wikipedia</a></div>");
-                //answer latitude
-                var latitude = answer_papy.latitude
-                //answer longitude
-                var longitude = answer_papy.longitude
-                //answer address display
-                $('#chat').append($answer_address);
-                setTimeout(function() {
-                    $('#chat').append($answer_wiki);
+                    console.log(data, status);
+                    //I get the question
+                    var question = $('#formtexte').val().replace("<", " ");
+                    //question display
+                    var $question = $("<div class='chat_right'><p>"+question+"</p></div>");
+                    //deletion of the form
+                    $('#form-question').get(0).reset();
+                    //adding the question to the screen
+                    $('#chat').append($question);
+                    //loader
+                    $('.loader').fadeIn();
+                    //adding the answer to the screen
+                    var answer_papy = JSON.parse(data);
+                    //answer api google map 
+                    var $answer_address = $("<div class='chat_left'><p>"+answer_papy.address+"</p></div>");
+                    //answer wikipedia
+                    var $answer_wiki = $("<div class='chat_left'><p>"+
+                        answer_papy.history+"</p><a href='https://fr.wikipedia.org/wiki/"+
+                        answer_papy.article_title+"'>Wikipedia</a></div>");
+                    //answer latitude
+                    var latitude = answer_papy.latitude
+                    //answer longitude
+                    var longitude = answer_papy.longitude
+                    //answer address display
+                    $('#chat').append($answer_address);
+                    setTimeout(function() {
+                        $('#chat').append($answer_wiki);
+                        //to display the last comment
+                        $(document).scrollTop($(document).height());
+                    }, 2000);
+                    //location
+                    var location = {lat: latitude, lng: longitude};
+                    // The map
+                    var map = new google.maps.Map(
+                        document.getElementById('map'), {zoom: 10, center: location});
+                    // The marker
+                    var marker = new google.maps.Marker({position: location, map: map});
                     //to display the last comment
                     $(document).scrollTop($(document).height());
-                  }, 2000);
-                //location
-                var location = {lat: latitude, lng: longitude};
-                // The map
-                var map = new google.maps.Map(
-                    document.getElementById('map'), {zoom: 10, center: location});
-                // The marker
-                var marker = new google.maps.Marker({position: location, map: map});
-                //to display the last comment
-                $(document).scrollTop($(document).height());
-                //Hide loader
-                $('.loader').fadeOut('fast');
-            });
+                    //Hide loader
+                    $('.loader').fadeOut('fast');
+                });
             };
         });
     });
